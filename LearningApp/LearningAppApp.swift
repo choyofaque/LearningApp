00377056a871cc6bc5ab40571b1a15e581a6d87b
Manuel Streit <manuel.streit@icloud.com>
2022-02-18 12:34:49 +0100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Manuel Streit on 18.02.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
