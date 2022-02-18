0bf0a465960855fd93541a830f7522239e901a47
Manuel Streit <manuel.streit@icloud.com>
2022-02-18 13:56:31 +0100

    Added JSON and Data Parsing

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,5 +9,54 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        
+        getLocalData()
+        
+    }
+    
+    func getLocalData() {
+        
+        let jsonURL = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            
+            let jsonData = try Data(contentsOf: jsonURL!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+            
+        }
+        catch {
+            
+            print("Could not parse local data!")
+            
+        }
+        
+        let styleURL = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            let styleData = try Data(contentsOf: styleURL!)
+            
+            self.styleData = styleData
+            
+            
+        }
+        
+        catch {
+            
+            print("Could not parse style data!")
+            
+        }
+        
+    }
     
 }
